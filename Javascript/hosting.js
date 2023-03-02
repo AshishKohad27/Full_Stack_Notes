49aad1cf1f2525de77fcd02e5a5f525e2b470f81
AshishKohad27 <kohadashish27@gmail.com>
2023-03-02 18:00:08 +0530

    hosting

---
+++ Javascript/hosting.js
@@ -0,0 +1,47 @@
+/*
+## "The javascript mechanism in which variables and function declarations are 
+moved to the top of their scope before execution of the code is called as Hoisting."
+
+
+## Execution context:
+   --> Global Executing Context
+   --> Local Executing Context
+
+   Executing Phases
+   1. Creation Phase
+   2. Execution Phase
+
+   State in hosting
+   1. Declaration
+   2. Initialization
+   3. Assignment
+
+   Variable Life Cycle
+   1. var 
+   2. let
+   3. const
+*/
+
+
+
+
+
+//phase
+
+//creation phase
+// 1. Declaration -->name
+
+// ---> TDZ <---
+
+//execution phase
+// 2. Initialization --->undefined   // 3. Assignment 
+
+
+
+
+
+
+console.log(name)
+let name = "ashish";
+
+
