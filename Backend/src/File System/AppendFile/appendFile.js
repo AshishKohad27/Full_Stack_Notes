7a2cf633638bc9e4dee5eb9176bfbcaf67722a42
AshishKohad27 <kohadashish27@gmail.com>
2023-03-17 14:23:36 +0530

    File System

--- Backend/src/File System/AppendFile/appendFile.js
+++ Backend/src/File System/AppendFile/appendFile.js
@@ -1,12 +1,12 @@
 // Create a new file using the appendFile() method:
 let fs = require("fs");
-fs.appendFile("fileName_5", "Hello File 5", (err) => {
+fs.appendFile("fileName_6", "Hello File 6", (err) => {
     if (err) throw err;
     console.log("NewFile Created Successfully!");
 });
 
 console.log("Before readFile")
-fs.readFile('./fileName_5',
+fs.readFile('./fileName_6',
     { encoding: 'utf8', flag: 'r' },
     (err, data) => {
         if (err)
